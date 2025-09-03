68b894a10393dc0842cc34f69b27e07e7551a597
Ziad-Mostafa28 <ziad46363@gmail.com>
2025-09-03 14:35:23 +0300

    feat: Update Footer component links and enhance hover styles

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -25,13 +25,14 @@ function Footer() {
             <span>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M5 20h14v-2H5m14-9h-4V3H9v6H5l7 7z" /></svg>
             </span>
-            Download Brochure
+            <Link to={'download-brochure'}>Download Broch</Link>
           </div>
           <div className={styles.item}>
             <span>
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3.464 16.828C2 15.657 2 14.771 2 11s0-5.657 1.464-6.828C4.93 3 7.286 3 12 3s7.071 0 8.535 1.172S22 7.229 22 11s0 4.657-1.465 5.828C19.072 18 16.714 18 12 18c-2.51 0-3.8 1.738-6 3v-3.212c-1.094-.163-1.899-.45-2.536-.96" /></svg>
             </span>
-            Contact Us
+            
+            <Link to={'contact-us'}>Contact Us</Link>
           </div>
         </div>
 
@@ -94,13 +95,13 @@ function Footer() {
           </div>
           <div className={styles.bottomFooter}>
             <p>© 2025 Al Sharkeya Sugar Manufacturing Company. All Right Reserved.</p>
-            <p className='d-flex gap-3'>Designed and Developed by <Link to='https://icon-creations.com/' target='_blank' ><img src="/img/homepage/icon.png" alt="" /></Link></p>
+            <p className='d-flex gap-3'><Link to='https://icon-creations.com/' target='_blank' ><img src="/img/homepage/icon.png" alt="" /></Link></p>
             <div className='d-flex gap-3'>
               <p>
-                <Link to='/'>Terms of Service</Link>
+                <Link to='terms-of-service'>Terms of Service</Link>
               </p>
               <p>
-                <Link to='/'>Privacy Policy</Link>
+                <Link to='privacy-policy'>Privacy Policy</Link>
               </p>
             </div>
           </div>
