28a6e98c8757d5f4720d1eda019dfa14f4d861d1
Ziad-Mostafa28 <ziad46363@gmail.com>
2025-10-12 15:56:52 +0300

    style: update colors for SustainabilityBox component images for better visual consistency

--- src/components/Responsibilities/Sustainability/SustainabilityBox/SustainabilityBox.jsx
+++ src/components/Responsibilities/Sustainability/SustainabilityBox/SustainabilityBox.jsx
@@ -4,12 +4,12 @@ import styles from "./SustainabilityBox.module.css";
 export default function SustainabilityBox() {
 
     const boxes = [
-        { img: '/img/responsibilities/Global1.png', text: 'Promoting responsible material use maximizing recycling, and optimizing industrial waste streams', color: '#F6E6D7' },
-        { img: '/img/responsibilities/Global2.png', text: 'Implementing efficient irrigation systems and wastewater treatment practices', color: '#C78E50' },
-        { img: '/img/responsibilities/Global3.png', text: 'Innovating industrial processes to enhance waste reduction and resource efficiency', color: '#00308F' },
-        { img: '/img/responsibilities/Global4.png', text: 'Promoting responsible material use, maximizing recycling, and optimizing industrial waste streams', color: '#336AD9' },
-        { img: '/img/responsibilities/Global5.png', text: 'Reducing emissions through waste-to-energy strategies and improved agricultural waste management', color: '#7CA429' },
-        { img: '/img/responsibilities/Global6.png', text: 'Conserving natural resources by minimizing landfill waste and promoting biodiversity', color: '#035034' },
+        { img: '/img/responsibilities/Global1.png', text: 'Promoting responsible material use maximizing recycling, and optimizing industrial waste streams', color: '#dda83a' },
+        { img: '/img/responsibilities/Global2.png', text: 'Implementing efficient irrigation systems and wastewater treatment practices', color: '#26bde2' },
+        { img: '/img/responsibilities/Global3.png', text: 'Innovating industrial processes to enhance waste reduction and resource efficiency', color: '#fd6925' },
+        { img: '/img/responsibilities/Global4.png', text: 'Promoting responsible material use, maximizing recycling, and optimizing industrial waste streams', color: '#bf8b2e' },
+        { img: '/img/responsibilities/Global5.png', text: 'Reducing emissions through waste-to-energy strategies and improved agricultural waste management', color: '#3f7e44' },
+        { img: '/img/responsibilities/Global6.png', text: 'Conserving natural resources by minimizing landfill waste and promoting biodiversity', color: '#56c02b' },
     ];
 
     return (
