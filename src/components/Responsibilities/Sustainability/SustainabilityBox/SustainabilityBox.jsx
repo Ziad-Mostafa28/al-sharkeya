77586db65a405ffdd2b69eb0d82399a86d4af652
Ahmed-Elshami <ahmed.n.alshamy@gmail.com>
2025-12-07 18:50:31 +0200

    feat: Update SustainabilityBox component for multilingual support and remove unused code

--- src/components/Responsibilities/Sustainability/SustainabilityBox/SustainabilityBox.jsx
+++ src/components/Responsibilities/Sustainability/SustainabilityBox/SustainabilityBox.jsx
@@ -9,23 +9,15 @@ export default function SustainabilityBox() {
         if (loading || !data?.data.sustainability_lists) return null;
         const  sustainability_mission_cards = data?.data?.sustainability_mission_cards;
 
-    // const boxes = [
-    //     { img: '/img/responsibilities/Global1.png', text: 'Promoting responsible material use maximizing recycling, and optimizing industrial waste streams', color: '#dda83a' },
-    //     { img: '/img/responsibilities/Global2.png', text: 'Implementing efficient irrigation systems and wastewater treatment practices', color: '#26bde2' },
-    //     { img: '/img/responsibilities/Global3.png', text: 'Innovating industrial processes to enhance waste reduction and resource efficiency', color: '#fd6925' },
-    //     { img: '/img/responsibilities/Global4.png', text: 'Promoting responsible material use, maximizing recycling, and optimizing industrial waste streams', color: '#bf8b2e' },
-    //     { img: '/img/responsibilities/Global5.png', text: 'Reducing emissions through waste-to-energy strategies and improved agricultural waste management', color: '#3f7e44' },
-    //     { img: '/img/responsibilities/Global6.png', text: 'Conserving natural resources by minimizing landfill waste and promoting biodiversity', color: '#56c02b' },
-    
-    
-    // ];
+  const lang = useSelector((state) => state.lang.lang);
+  const isArabic= lang === 'ar';  
 
     
 
     return (
       <section className={styles.SustainabilityBox}>
   <div className={styles.container}>
-    <h2>Sustainable Development Goals</h2>
+    <h2> {isArabic?'أهداف التنمية المستدامة':'Sustainable Development Goals'} </h2>
 
     <div className="row gy-4 mt-5 justify-content-center">
       { sustainability_mission_cards.map((box, index) => (
